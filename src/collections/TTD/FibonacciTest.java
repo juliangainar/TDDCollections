0306e65133687fc555d7937ba1884960bece342c
iuliangainar <gainar.ionut@gmail.com>
2023-02-26 13:51:27 +0100

    Test sur 0 qui échoue

--- src/collections/TTD/FibonacciTest.java
+++ src/collections/TTD/FibonacciTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 
 public class FibonacciTest {
     // on veut une fonction qui retourne la suite de Fibonacci jusqu'une certaine borne
-    // on testera ici la fonction
+    // on testera ici la fonction 
 
     @Test
     void testFibo1(){
-
+        assertEquals("0", Fibonnaci.calcul(0));
     }
 }
